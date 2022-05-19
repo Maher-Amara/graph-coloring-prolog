77f2b18d14de980a7b334a63adc887b9f4254fdd
Maher AMARA <maheramara32@yahoo.fr>
2022-05-19 22:08:43 +0100

    final version

--- main.pl
+++ main.pl
@@ -1,10 +1,3 @@
-% definition du graphe
-adj(1,2). adj(1,3). adj(1,4). adj(1,5).
-adj(2,3). adj(2,4).
-adj(3,4).
-adj(4,5).
-adjacent(X,Y):-adj(X,Y);adj(Y,X).
-
 % colorage 1
 color(1,blue,c1). color(2,red,c1). color(3,green,c1). color(4,yellow,c1). color(5,red,c1).
 % colorage 2
@@ -13,9 +6,6 @@ color(1,green,c2). color(2,red,c2). color(3,green,c2). color(4,yellow,c2). color
 color(1,blue,c3). color(2,blue,c3). color(3,blue,c3). color(4,blue,c3). color(5,blue,c3).
 % colorage 4
 color(1,green,c4). color(2,red,c4). color(3,yellow,c4). color(4,blue,c4). color(5,yellow,c4).
-
-% definition des couleurs
-couleur( vert ). couleur( bleu ). couleur( rouge ). couleur( violet ).
 
 % vérifier si un coloriage est valide:
 conflictColorageGraphe(Colorage):-adjacent(Region1,Region2),color(Region1,Color,Colorage),color(Region2,Color,Colorage).
@@ -27,7 +17,15 @@ conflictColorage(Region1,Region2,Colorage):-adjacent(Region1,Region2),color(Regi
 % -----------------------------------------
 %            Mini-Project
 % -----------------------------------------
+% definition du graphe
+adj(1,2). adj(1,3). adj(1,4). adj(1,5).
+adj(2,3). adj(2,4).
+adj(3,4).
+adj(4,5).
+adjacent(X,Y):-adj(X,Y);adj(Y,X).
 
+% definition des couleurs
+couleur( vert ). couleur( bleu ). couleur( rouge ). couleur( jaune ).
 
 % trouver les regions en conflit:
 conflit(couleur( Region1, Couleur ), couleur( Region2, Couleur )) :-
