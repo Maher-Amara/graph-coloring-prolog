8c6873e580f7ada84b92efedd72cc4e6631f5f87
Maher AMARA <maheramara32@yahoo.fr>
2022-05-19 15:40:26 +0100

    mini project

--- main.pl
+++ main.pl
@@ -1,53 +1,33 @@
 % definition du graphe
-adj(1,2).
-adj(1,3).
-adj(1,4).
-adj(1,5).
-adj(2,3).
-adj(2,4).
+adj(1,2). adj(1,3). adj(1,4). adj(1,5).
+adj(2,3). adj(2,4).
 adj(3,4).
 adj(4,5).
 adjacent(X,Y):-adj(X,Y);adj(Y,X).
 
 % colorage 1
-color(1,blue,c1).
-color(2,red,c1).
-color(3,green,c1).
-color(4,yellow,c1).
-color(5,red,c1).
-
+color(1,blue,c1). color(2,red,c1). color(3,green,c1). color(4,yellow,c1). color(5,red,c1).
 % colorage 2
-color(1,green,c2).
-color(2,red,c2).
-color(3,green,c2).
-color(4,yellow,c2).
-color(5,green,c2).
-
+color(1,green,c2). color(2,red,c2). color(3,green,c2). color(4,yellow,c2). color(5,green,c2).
 % colorage 3
-color(1,blue,c3).
-color(2,blue,c3).
-color(3,blue,c3).
-color(4,blue,c3).
-color(5,blue,c3).
-
+color(1,blue,c3). color(2,blue,c3). color(3,blue,c3). color(4,blue,c3). color(5,blue,c3).
 % colorage 4
-color(1,green,c4).
-color(2,red,c4).
-color(3,yellow,c4).
-color(4,blue,c4).
-color(5,yellow,c4).
+color(1,green,c4). color(2,red,c4). color(3,yellow,c4). color(4,blue,c4). color(5,yellow,c4).
 
 % definition des couleurs
-couleur( vert ).
-couleur( bleu ).
-couleur( rouge ).
-couleur( violet ).
+couleur( vert ). couleur( bleu ). couleur( rouge ). couleur( violet ).
 
 % vérifier si un coloriage est valide:
-conflictGraphe(C):-adjacent(X,Y),color(X,Color,C),color(Y,Color,C).
+conflictColorageGraphe(Colorage):-adjacent(Region1,Region2),color(Region1,Color,Colorage),color(Region2,Color,Colorage).
 
 % connaître les régions adjacentes qui ont la même couleur:
-conflictColorage(X,Y,C):-adjacent(X,Y),color(X,Color,C),color(Y,Color,C).
+conflictColorage(Region1,Region2,Colorage):-adjacent(Region1,Region2),color(Region1,Color,Colorage),color(Region2,Color,Colorage).
+
+
+% -----------------------------------------
+%            Mini-Project
+% -----------------------------------------
+
 
 % trouver les regions en conflit:
 conflit(couleur( Region1, Couleur ), couleur( Region2, Couleur )) :-
